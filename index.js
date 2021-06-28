6f20c85c89cfb4a33e937c502ac8324041fec621
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-06-28 23:17:52 +0300

    Добавляет массив

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const fruitsQuent = document.querySelector('list');
+const fruitsQuent = Array.from(document.querySelectorAll('li'));
+//console.log(fruitsQuent);
+//console.dir(fruitsQuent);
+// const a = [...fruitsQuent].length;
+// console.log(a);
+//console.log(Array.from(fruitsQuent));
 console.log(fruitsQuent);
+console.log(fruitsQuent.length);
+console.log(Array.from(document.querySelector('ul')));
